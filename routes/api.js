9c02c6bcd1843001be94757d3d009f407d053563
Marvin <marvinjunaidi_2327240003@mhs.mdp.ac.id>
2025-10-22 20:38:56 +0700

    authmiddleware

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,12 @@
 import express from "express"
 import * as authController from "../contoller/authController.js"
+import * as profileController from "../contoller/profileController.js"
+import { protect } from "../middleware/authMiddleware.js";
 
 const api = express.Router();
 
 api.post("/register", authController.register);
 api.post("/login", authController.login);
+api.get("/me", protect ,profileController.privateProfile);
 
 export default api
